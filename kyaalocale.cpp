bfc4e64a7ea64684b4fbb8e6231474ae12075d07
Michael <mekim2015@ymail.com>
2013-04-25 20:32:28 +0300

    Update kyaalocale.cpp
    Position of comments fixed.

--- kyaalocale.cpp
+++ kyaalocale.cpp
@@ -9,20 +9,20 @@ BOOL CALLBACK MyFuncLocaleEx(LPWSTR pStr, DWORD dwFlags, LPARAM lparam)
     WCHAR** argv = (WCHAR**)lparam;
     WCHAR wcBuffer[BUFFER_SIZE];
     int iResult;
-  // Get its LCID 
+    // Get its LCID 
     LCID lcid = LocaleNameToLCID(pStr, NULL);
     if (lcid != 0)
         wprintf(L"LCID for %s is %x\n", pStr, lcid);
     else
         wprintf(L"Error %d getting LCID\n", GetLastError());
-	// Print out the locale name we found
+    // Print out the locale name we found
     iResult = GetLocaleInfoEx(pStr, LOCALE_SENGLANGUAGE, wcBuffer, BUFFER_SIZE);
-	// If it succeeds, print it out
+    // If it succeeds, print it out
     if (iResult > 0)
         wprintf(L"Locale %s (%s)\n", pStr, wcBuffer);
     else
         wprintf(L"Locale %s had error %d\n", pStr, GetLastError());
-	// CompareStringEx if this is the system locale, let us know
+    // CompareStringEx if this is the system locale, let us know
     iResult = GetSystemDefaultLocaleName(wcBuffer, BUFFER_SIZE);
     if (iResult > 0)
     {
@@ -50,15 +50,15 @@ return (TRUE);
 int __cdecl wmain(int argc, wchar_t* argv[])
 {
     UNREFERENCED_PARAMETER(argc);
-	// Enumerate all the locales and report on them
+    // Enumerate all the locales and report on them
     EnumSystemLocalesEx( MyFuncLocaleEx, LOCALE_ALL, (LPARAM)argv, NULL);
-	// See which of the input locales are valid (if any)
+    // See which of the input locales are valid (if any)
     if (*argv && argv[1])
     {
-		// Check each argument to see if our locale matches
+	// Check each argument to see if our locale matches
         for (int i = 1; argv[i] != 0; i++)
         {
-			// See if this is a valid locale name 
+	    // See if this is a valid locale name 
             if (!IsValidLocaleName(argv[i]))
             {
                 wprintf(L"%s is not a valid locale name\n", argv[i]);
