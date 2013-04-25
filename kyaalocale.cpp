3ea3578f9d484881ae7fadf1fcf6ac5826db16b0
Michael <mekim2015@ymail.com>
2013-04-25 20:14:24 +0300

    Create kyaalocale.cpp
    Cleaned the code a bit.

---
+++ kyaalocale.cpp
@@ -0,0 +1,69 @@
+#include "stdafx.h"
+#include "windows.h"
+#include "stdio.h"
+// Callback for EnumSystemLocalesEx()
+#define BUFFER_SIZE 512
+BOOL CALLBACK MyFuncLocaleEx(LPWSTR pStr, DWORD dwFlags, LPARAM lparam)
+{
+    UNREFERENCED_PARAMETER(dwFlags);
+    WCHAR** argv = (WCHAR**)lparam;
+    WCHAR wcBuffer[BUFFER_SIZE];
+    int iResult;
+  // Get its LCID 
+    LCID lcid = LocaleNameToLCID(pStr, NULL);
+    if (lcid != 0)
+        wprintf(L"LCID for %s is %x\n", pStr, lcid);
+    else
+        wprintf(L"Error %d getting LCID\n", GetLastError());
+	// Print out the locale name we found
+    iResult = GetLocaleInfoEx(pStr, LOCALE_SENGLANGUAGE, wcBuffer, BUFFER_SIZE);
+	// If it succeeds, print it out
+    if (iResult > 0)
+        wprintf(L"Locale %s (%s)\n", pStr, wcBuffer);
+    else
+        wprintf(L"Locale %s had error %d\n", pStr, GetLastError());
+	// CompareStringEx if this is the system locale, let us know
+    iResult = GetSystemDefaultLocaleName(wcBuffer, BUFFER_SIZE);
+    if (iResult > 0)
+    {
+        if (CompareStringEx(LOCALE_NAME_INVARIANT,
+                            LINGUISTIC_IGNORECASE,
+                            wcBuffer,
+                            -1,
+                            pStr,
+                            -1,
+                            NULL,
+                            NULL,
+                            0) == CSTR_EQUAL)
+        {
+            wprintf(L"\nLocale %s is the system locale!\n", wcBuffer);
+            system("Pause");
+        }
+    }
+    else
+    {
+        wprintf(L"Error %d getting system locale\n", GetLastError());
+        system("Pause");
+    }
+return (TRUE);
+}
+int __cdecl wmain(int argc, wchar_t* argv[])
+{
+    UNREFERENCED_PARAMETER(argc);
+	// Enumerate all the locales and report on them
+    EnumSystemLocalesEx( MyFuncLocaleEx, LOCALE_ALL, (LPARAM)argv, NULL);
+	// See which of the input locales are valid (if any)
+    if (*argv && argv[1])
+    {
+		// Check each argument to see if our locale matches
+        for (int i = 1; argv[i] != 0; i++)
+        {
+			// See if this is a valid locale name 
+            if (!IsValidLocaleName(argv[i]))
+            {
+                wprintf(L"%s is not a valid locale name\n", argv[i]);
+                system("Pause");
+            }
+        }
+    }
+}
